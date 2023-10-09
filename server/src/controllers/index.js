d6dd6f2cc037a0e1f6a03a38ad3493d5b620355a
Cacho Medina <zanlorenzocacho10@gmail.com>
2023-10-09 18:47:14 -0300

    modifying getDrivers

--- server/src/controllers/index.js
+++ server/src/controllers/index.js
@@ -1,9 +1,14 @@
 const axios = require("axios");
+const url = "http://localhost:5000/drivers";
 
 const getDrivers = async (req, res) => {
-    const result = await axios.get("http://localhost:5000/drivers");
-    const data = result.data;
-    res.json(data);
+    try {
+        const result = await axios(url);
+        const data = result.data;
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 };
 
 module.exports = {
