ffe16e1771d1ac7b06a7a296abadc2f30b3a6517
Cacho Medina <zanlorenzocacho10@gmail.com>
2023-10-10 16:38:10 -0300

    configurando rutas

--- server/src/controllers/index.js
+++ server/src/controllers/index.js
@@ -1,6 +1,37 @@
 const axios = require("axios");
 
 const getDrivers = async (req, res) => {
+    try {
+        const result = await axios.get("http://localhost:5000/drivers");
+        const data = result.data;
+        res.json(data);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+const getDriver = async (req, res) => {
+    try {
+        const { idDriver } = req.params;
+        const result = await axios(
+            `http://localhost:5000/drivers/${parseInt(idDriver)}`
+        );
+        const data = result.data;
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+const getDriversByName = async (req, res) => {
+    const result = await axios.get("http://localhost:5000/drivers");
+    const data = result.data;
+    res.json(data);
+};
+const getTeams = async (req, res) => {
+    const result = await axios.get("http://localhost:5000/drivers");
+    const data = result.data;
+    res.json(data);
+};
+const postDriver = async (req, res) => {
     const result = await axios.get("http://localhost:5000/drivers");
     const data = result.data;
     res.json(data);
@@ -8,4 +39,8 @@ const getDrivers = async (req, res) => {
 
 module.exports = {
     getDrivers,
+    getDriver,
+    getDriversByName,
+    getTeams,
+    postDriver,
 };
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,8 +1,18 @@
 const { Router } = require("express");
-const { getDrivers } = require("../controllers");
+const {
+    getDrivers,
+    getDriver,
+    getDriversByName,
+    getTeams,
+    postDriver,
+} = require("../controllers");
 
 const router = Router();
 
-router.get("/", getDrivers);
+router.get("/drivers", getDrivers);
+router.get("/drivers/:idDriver", getDriver);
+router.get("/drivers", getDriversByName);
+router.get("/teams", getTeams);
+router.post("/drivers", postDriver);
 
 module.exports = router;
